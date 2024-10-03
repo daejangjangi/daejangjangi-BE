dd07e30992b5393957edde9e730dbc0b49a6ca8c
hyensukim <crazymin17@naver.com>
2024-10-04 02:05:35 +0900

    [#5] base64 인코딩/디코딩 API 구현 - application-secret.yml 인코딩 및 디코딩을 위한 기능

--- src/main/java/com/daejangjangi/backend/basic/BasicController.java
+++ src/main/java/com/daejangjangi/backend/basic/BasicController.java
@@ -4,11 +4,16 @@ import com.daejangjangi.backend.global.exception.ClientDataException;
 import com.daejangjangi.backend.global.exception.ForbiddenException;
 import com.daejangjangi.backend.global.exception.UnAuthenticatedException;
 import com.daejangjangi.backend.global.response.ApiGlobalResponse;
+import com.daejangjangi.backend.global.utils.Base64Util;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class BasicController {
 
   @GetMapping("/health-check")
@@ -25,5 +30,15 @@ public class BasicController {
       case "INTERNAL_SERVER_ERROR" -> throw new RuntimeException("INTERNAL_SERVER_ERROR");
       default -> throw new RuntimeException("default");
     }
+  }
+
+  @PostMapping("/base64/encode")
+  public ApiGlobalResponse<String> base64Encoding(@RequestBody String data) {
+    return ApiGlobalResponse.ok(Base64Util.encode(data));
+  }
+
+  @PostMapping("/base64/decode")
+  public ApiGlobalResponse<String> base64Decoding(@RequestBody String data) {
+    return ApiGlobalResponse.ok(Base64Util.decode(data));
   }
 }
