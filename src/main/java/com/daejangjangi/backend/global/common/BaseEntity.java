14040bdb8ab3b4b07c72d40b44ac18543d9a6fff
jiruen <kjr2699@naver.com>
2024-10-16 14:23:02 +0900

    [#14] BaseEntity 어노테이션 수정
    - 수정일 어노테이션 수정 (@LastModifiedBy -> @LastModifiedDate)

--- src/main/java/com/daejangjangi/backend/global/common/BaseEntity.java
+++ src/main/java/com/daejangjangi/backend/global/common/BaseEntity.java
@@ -5,7 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -19,7 +19,7 @@ public class BaseEntity {
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedBy
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
 }
