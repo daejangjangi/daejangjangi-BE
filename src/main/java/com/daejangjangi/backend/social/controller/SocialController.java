6375002f2e74bc2a91b30f1e45bd16139183b734
hyensukim <crazymin17@naver.com>
2024-10-17 13:22:38 +0900

    [#17] 회원 관련 API Swagger 문서화

--- src/main/java/com/daejangjangi/backend/social/controller/SocialController.java
+++ src/main/java/com/daejangjangi/backend/social/controller/SocialController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/socials")
 @RequiredArgsConstructor
-public class SocialController {
+public class SocialController implements SocialApi {
 
   private final SocialService socialService;
   private final SocialValidator socialValidator;
