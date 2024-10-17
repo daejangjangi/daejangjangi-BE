7f79344a5686e926de8543c8b9387cb211716a33
hyensukim <crazymin17@naver.com>
2024-10-18 00:28:59 +0900

    Swagger 스키마 추가

--- src/main/java/com/daejangjangi/backend/member/domain/dto/MemberRequestDto.java
+++ src/main/java/com/daejangjangi/backend/member/domain/dto/MemberRequestDto.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class MemberRequestDto {
 
+  @Schema(description = "회원가입 DTO")
   public record Join(
       @Schema(description = "이메일")
 
@@ -42,18 +43,23 @@ public class MemberRequestDto {
       @Pattern(regexp = "^[mw]+$", message = "성별은 m(남성) 또는 w(여성)를 입력해야 합니다.")
       String gender,
 
-      @Schema(description = "생년월일")
+      @Schema(description = "생년월일", example = "2024-10-17")
 
       @NotNull(message = "생일을 입력하세요.")
       LocalDate birth,
 
-      @Schema(description = "회원 장질환")
+      @Schema(description = "회원 장질환", allowableValues = {
+          "변비", "과민성장증후군_설사형", "과민성장증후군_변비형", "치질", "치핵", "치열", "변실금", "항문소양증",
+          "대장암", "크론병", "궤양성대장염", "복부팽만", "없음"
+      })
 
       @NotNull(message = "질병을 선택해주세요.")
       @Size(min = 1, message = "질병을 최소 {min}개 이상 선택 바랍니다.")
       List<String> diseases,
 
-      @Schema(description = "회원 관심 상품 카테고리")
+      @Schema(description = "회원 관심 상품 카테고리", allowableValues = {
+          "유산균", "식이섬유", "저포드맵", "비건", "기타 장건강 간식"
+      })
 
       @NotNull(message = "관심 상품을 선택해주세요.")
       @Size(min = 1, message = "관심 상품을 최소 {min}개 이상 선택 바랍니다.")
@@ -62,6 +68,7 @@ public class MemberRequestDto {
 
   }
 
+  @Schema(description = "로그인 DTO")
   public record Login(
 
       @Schema(description = "이메일")
