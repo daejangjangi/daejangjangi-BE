777e2740d6a30b5d8e1682a9cb47ed9ffc3514cf
jiruen <kjr2699@naver.com>
2024-11-14 18:58:44 +0900

    [#52] 회원 - 게시글 댓글 좋아요 엔티티 양방향 매핑 관계이므로 불필요한 메서드 삭제

--- src/main/java/com/daejangjangi/backend/member/service/MemberService.java
+++ src/main/java/com/daejangjangi/backend/member/service/MemberService.java
@@ -4,7 +4,6 @@ import com.daejangjangi.backend.board.domain.entity.Board;
 import com.daejangjangi.backend.board.repository.BoardRepository;
 import com.daejangjangi.backend.category.domain.Category;
 import com.daejangjangi.backend.disease.domain.Disease;
-import com.daejangjangi.backend.like.domain.entity.PostCommentLike;
 import com.daejangjangi.backend.member.domain.entity.Member;
 import com.daejangjangi.backend.member.domain.entity.MemberBoard;
 import com.daejangjangi.backend.member.domain.entity.MemberCategory;
@@ -20,8 +19,6 @@ import com.daejangjangi.backend.member.repository.MemberDiseaseRepository;
 import com.daejangjangi.backend.member.repository.MemberRepository;
 import com.daejangjangi.backend.post.domain.entity.Post;
 import com.daejangjangi.backend.post.exception.NotManagedBoardException;
-import com.daejangjangi.backend.post.repository.PostCommentLikeRepository;
-import com.daejangjangi.backend.post.repository.PostCommentRepository;
 import com.daejangjangi.backend.post.repository.PostLikeRepository;
 import com.daejangjangi.backend.post.repository.PostRepository;
 import com.daejangjangi.backend.token.domain.entity.Token;
@@ -53,7 +50,6 @@ public class MemberService implements UserDetailsService {
   private final MemberBoardRepository memberBoardRepository;
   private final BoardRepository boardRepository;
   private final BCryptPasswordEncoder passwordEncoder;
-  private final PostCommentLikeRepository postCommentLikeRepository;
   private final PostRepository postRepository;
   private final PostLikeRepository postLikeRepository;
 
@@ -200,12 +196,10 @@ public class MemberService implements UserDetailsService {
   /**
    * 회원탈퇴
    */
-  @Transactional
   public void withdraw() {
     Long memberId = getCurrentId();
     Member member = findById(memberId);
     Token token = getToken(memberId);
-    removeCommentLikes(member);
     removePostLikes(member);
     memberRepository.delete(member);
     // TODO : token - member 연관관계 수정하여 CASCADE 로 삭제하도록 수정
@@ -439,18 +433,6 @@ public class MemberService implements UserDetailsService {
   private Token getToken(Long memberId) {
     return tokenRepository.findByMemberId(memberId)
         .orElseThrow(NotFoundMemberException::new);
-  }
-
-  /**
-   * 회원이 좋아요한 댓글 좋아요 삭제
-   *
-   * @param member 회원 정보
-   */
-  private void removeCommentLikes(Member member) {
-    List<PostCommentLike> postCommentLikes = postCommentLikeRepository.findByMember(member);
-    if (!postCommentLikes.isEmpty()) {
-      postCommentLikeRepository.deleteAll(postCommentLikes);
-    }
   }
 
   /**
