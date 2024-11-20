972b9e7743a5ebda72bcb0f68acf373b20741a9a
jiruen <kjr2699@naver.com>
2024-11-20 17:22:17 +0900

    [#58] fcm 토큰 저장, 삭제 로직 구현

---
+++ src/main/java/com/daejangjangi/backend/fcm/repository/FcmTokenRepository.java
@@ -0,0 +1,18 @@
+package com.daejangjangi.backend.fcm.repository;
+
+import com.daejangjangi.backend.fcm.domain.entity.FcmToken;
+import com.daejangjangi.backend.member.domain.entity.Member;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
+
+  Optional<FcmToken> findByMemberAndToken(Member member, String token);
+
+  void deleteByToken(String fcmToken);
+
+  void deleteByMember(Member member);
+
+  List<FcmToken> findByMember(Member member);
+}
