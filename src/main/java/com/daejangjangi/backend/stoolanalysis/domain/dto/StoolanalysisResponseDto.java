fea5e5ec420d44c8ba168ddba5fae45456e33eac
jiruen <kjr2699@naver.com>
2024-12-11 14:38:35 +0900

    [#82] @JsonProperty 사용하여 필드명 카멜케이스로 통일

--- src/main/java/com/daejangjangi/backend/stoolanalysis/domain/dto/StoolanalysisResponseDto.java
+++ src/main/java/com/daejangjangi/backend/stoolanalysis/domain/dto/StoolanalysisResponseDto.java
@@ -4,6 +4,7 @@ import com.daejangjangi.backend.stoolanalysis.domain.enums.Mucus;
 import com.daejangjangi.backend.stoolanalysis.domain.enums.ProteinLumps;
 import com.daejangjangi.backend.stoollog.domain.enums.Color;
 import com.daejangjangi.backend.stoollog.domain.enums.Form;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 
@@ -45,11 +46,14 @@ public class StoolanalysisResponseDto {
 
   @Schema(name = "StoolDiagnosticAiResultResponse", description = "배변 분석 AI 진단 응답 DTO")
   public record StoolDiagnosticAiResult(
+
       @Schema(description = "배변 진단 결과",
           example = "보호자님께,\\n\\n아기의 변에 대한 정보를 주셔서 감사합니다.  "
               + "아이보리색의 딱딱한 변에 피가 섞여 있고, 점액이나 단백질 덩어리는 없으며, "
               + "모유만 먹는다는 설명을 바탕으로 몇 가지 가능성을 고려해 볼 필요가 있습니다... ")
-      String user_language
+      @JsonProperty("user_language")
+      String userLanguage
+
   ) {
 
   }
@@ -61,8 +65,12 @@ public class StoolanalysisResponseDto {
               + "아이보리색의 딱딱한 변에 피가 섞여 있고, 점액이나 단백질 덩어리는 없으며, "
               + "모유만 먹는다는 설명을 바탕으로 몇 가지 가능성을 고려해 볼 필요가 있습니다... ")
       String result,
+
+      @Schema(description = "배변 분석일")
       LocalDateTime date,
+      @Schema(description = "배변 묽기")
       Form form,
+      @Schema(description = "배변 색상")
       Color color
   ) {
 
